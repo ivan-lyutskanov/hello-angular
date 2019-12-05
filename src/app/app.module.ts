1b523761d49e920cc32928f24af4702b6f966ada
ivan.lyutsknaov <ivan.lyutskanov@scalefocus.com>
2019-12-05 16:09:58 +0200

    'init'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../services/todo.service';
+
+import { Todo } from '../models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.scss']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+  }
+
+  toggleCompleted() {
+    this.todo.completed = !this.todo.completed;
+    this.todoService.updateCompleted(this.todo).subscribe( t => {
+      console.log(t);
+    });
+  }
+
+  deleteItem() {
+    this.deleteTodo.emit(this.todo);
+  }
+
+}
